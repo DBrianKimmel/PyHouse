11527cc4d239423cfad76bc1100d12b2ef21115d
DBrianKimmel <d.briankimmel@gmail.com>
2014-01-02 12:32:58 -0500

    Start schedule/auto_update

---
+++ src/scheduling/auto_update.py
@@ -0,0 +1,14 @@
+"""
+Created on Dec 31, 2013
+
+@author: briank
+
+This module checks with something central and updates the PyHouse software if an update is needed.
+"""
+
+class API(object):
+    """
+    """
+
+
+# ## END DBK
