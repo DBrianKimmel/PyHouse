f0f75a6570b2f029eb639fd3d0cd4191d5ff2640
DBrianKimmel <dbriankimmel@gmail.com>
2014-04-07 20:19:16 -0400

    debug

--- src/core/node_domain.py
+++ src/core/node_domain.py
@@ -20,6 +20,7 @@ from twisted.internet.protocol import Factory
 from twisted.protocols.amp import AMP, Integer, String, Command
 
 # from src.core.nodes import NodeData
+from src.utils.tools import PrintBytes
 
 
 g_debug = 1
@@ -77,6 +78,7 @@ class RegisterNodeError(Exception):
 class RegisterNode(Command):
     """
     """
+    requiresAnswer = True
     arguments = [('Command', Integer()),
                  ('NodeName', String()),
                  ('NodeType', Integer()),
@@ -89,7 +91,7 @@ class RegisterNode(Command):
 class AmpClientProtocol(AMP):
 
     def dataReceived(self, p_data):
-        l_msg = 'Amp Client Received {0:}'.format(p_data)
+        l_msg = 'Domain Client - Received {0:}'.format(PrintBytes(p_data))
         g_logger.debug(l_msg)
 
     def connectionMade(self):
@@ -97,34 +99,34 @@ class AmpClientProtocol(AMP):
         Send them our node info
         Ask for their Node info.
         """
-        g_logger.debug('Amp Client connection made.')
+        g_logger.debug('Domain Client - connection made.')
 
 
 class AmpClientFactory(Factory):
 
     def startedConnecting(self, p_connector):
-        g_logger.info("AMP Client startedConnecting {0:}".format(p_connector))
+        g_logger.info("Domain Client - startedConnecting {0:}".format(p_connector))
 
     def buildProtocol(self, _addr):
         return AmpClientProtocol()
 
     def clientConnectionLost(self, _connector, p_reason):
-        g_logger.error('AmpClientFactory - lost connection {0:}'.format(p_reason))
+        g_logger.error('DomainClientFactory - lost connection {0:}'.format(p_reason))
 
     def clientConnectionFailed(self, _connector, p_reason):
-        g_logger.error('AmpClientFactory - Connection failed {0:}'.format(p_reason))
+        g_logger.error('DomainClientFactory - Connection failed {0:}'.format(p_reason))
 
 
 class AmpClient(object):
 
+
     def connect(self, p_pyhouses_obj, p_address):
-        l_endpoint = TCP4ClientEndpoint(p_pyhouses_obj.Reactor, p_address, AMP_PORT)
-        l_factory = AmpClientFactory()
-        l_defer = l_endpoint.connect(l_factory)
-        g_logger.info("Domain Client started to address {0:}".format(p_address))
+        def cb_show_result(p_dict):
+            g_logger.debug("Domain Client - Got result {0:}".format(p_dict))
+
         def cb_send_register_node(p_protocol):
-            g_logger.debug('Sending our local node to a new discovered address {0:}'.format(p_address))
-            p_protocol.callRemote(
+            g_logger.debug('Domain Client - Sending our local node to a new discovered address {0:}'.format(p_address))
+            l_defer2 = p_protocol.callRemote(
                 RegisterNode,
                 Command = 1,
                 NodeName = p_pyhouses_obj.CoreData.Nodes[0].Name,
@@ -132,24 +134,32 @@ class AmpClient(object):
                 IPv4 = '3.5.7.9',
                 IPv6 = 'ff00::'
                 )
-        l_defer.addCallback(cb_send_register_node)
+            l_defer2.addCallback(cb_show_result)
+
         def cb_registered(p_result):
-            g_logger.debug('Registration result:{0:}'.format(p_result))
-        l_defer.addCallback(cb_registered)
+            g_logger.debug('Domain Client - Registration result:{0:}'.format(p_result))
+
         def eb_error(p_error):
-            g_logger.debug('Registration error:{0:}'.format(p_error))
-        l_defer.addErrback(eb_error)
-        def cb_done(_ignored):
-            g_logger.debug('Registration done')
-        l_defer.addCallback(cb_done)
-        return l_defer
+            g_logger.debug('Domain Client - Registration error:{0:}'.format(p_error))
+
+        def cb_done(p_done):
+            g_logger.debug('Domain Client - Registration done:{0:}'.format(p_done))
+
+        l_endpoint = TCP4ClientEndpoint(p_pyhouses_obj.Reactor, p_address, AMP_PORT)
+        l_factory = AmpClientFactory()
+        l_defer1 = l_endpoint.connect(l_factory)
+        g_logger.info("Domain Client started to address {0:}".format(p_address))
+        l_defer1.addCallback(cb_send_register_node)
+        l_defer1.addCallback(cb_registered)
+        l_defer1.addErrback(eb_error)
+        l_defer1.addCallback(cb_done)
+        return l_defer1
 
 
 class AmpServerProtocol(AMP):
 
     def dataReceived(self, p_data):
-
-        g_logger.debug('Amp Server data rxed {0:}'.format(p_data))
+        g_logger.debug('Domain Server data rxed {0:}'.format(PrintBytes(p_data)))
 
     def connectionMade(self):
         """Somebody connected to us...
