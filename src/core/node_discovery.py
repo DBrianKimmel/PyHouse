a1f7630d1efe3cff4e12d8e9cfdab1cb00b29d08
DBrianKimmel <dbriankimmel@gmail.com>
2014-04-10 13:08:06 -0400

    WIP

--- src/core/node_discovery.py
+++ src/core/node_discovery.py
@@ -118,10 +118,11 @@ class MulticastDiscoveryClientProtocol(ConnectedDatagramProtocol):
         self.transport.write(WHOS_THERE, (PYHOUSE_MULTICAST, PYHOUSE_DISCOVERY_PORT))
 
     def datagramReceived(self, p_datagram, p_address):
-        self.m_pyhouses_obj.CoreData.Nodes[0].ConnectionAddr = p_address[0]
-        # g_logger.info("Client Discovery Datagram {0:} received from {1:}".format(repr(p_datagram), repr(p_address)))
-        g_logger.debug("Update node 0 address to {0:}, {1:}".format(p_address[0], p_datagram))
-        pass
+        if p_datagram == WHOS_THERE:
+            self.m_pyhouses_obj.CoreData.Nodes[0].ConnectionAddr = p_address[0]
+            # g_logger.info("Client Discovery Datagram {0:} received from {1:}".format(repr(p_datagram), repr(p_address)))
+            g_logger.debug("Update node 0 address to {0:}, {1:}".format(p_address[0], p_datagram))
+            pass
 
 
 class Utility(object):
