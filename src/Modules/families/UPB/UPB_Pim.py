536bb5a699959c5fd5102fd4e86c37e7babb2e9b
DBrianKimmel <dbriankimmel@gmail.com>
2014-07-23 17:47:10 -0400

    Debug

--- src/Modules/families/UPB/UPB_Pim.py
+++ src/Modules/families/UPB/UPB_Pim.py
@@ -378,6 +378,8 @@ class PimDriverInterface(DecodeResponses):
         self.m_pyhouse_obj.Twisted.Reactor.callLater(RECEIVE_TIMEOUT, self.receive_loop, p_controller_obj)
         if p_controller_obj._DriverAPI != None:
             l_msg = p_controller_obj._DriverAPI.Read()
+            if g_debug >= 1:
+                LOG.debug('Fetched message  {0:}'.format(PrintBytes(l_msg)))
             if len(l_msg) == 0:
                 return
             self.decode_response(p_controller_obj)
