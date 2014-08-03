d225302667ada2f135fa35d1ae77525054869bd3
DBrianKimmel <dbriankimmel@gmail.com>
2014-08-03 18:20:50 -0400

    Work on Node-domain

--- src/Modules/Core/node_domain.py
+++ src/Modules/Core/node_domain.py
@@ -76,79 +76,122 @@ class GetNodeList(Command):
     response = [('Nodes', AmpList([('x', String())]))]
 
 
-class DomainBoxDispatcher_AmpProtocol(AMP):
+class SendNodeList(Command):
+    """ Send a list of all the nodes.
+    """
+    commandName = 'getNodelist'
+    arguments = [('length', Integer()),
+                 ('Nodes', AmpList([('x', String())]))
+                 ]
+    response = [('Length', Integer())]
+
+
+class DomainAmp(AMP):
     """
     AMP is a subclass of (BinaryBoxProtocol, BoxDispatcher, CommandLocator, SimpleStringLocator)
     """
 
     m_amp = None
-    m_pyhouse_obj = None
-    m_transport = None
+
 
     def __init__(self, p_pyhouse_obj):
         """
         @param p_address: is a 3-tupple (AddressFamily, IPv4Addr, Port)
         """
+        super(DomainAmp, self).__init__()
         self.m_pyhouse_obj = p_pyhouse_obj
+        self.m_transport = None
         self.m_amp = self
         if g_debug >= 1:
-            LOG.debug('==095== Dispatch DomainBoxDispatcher_AmpProtocol()')
+            LOG.debug('==106== DomainAmp.__init__()')
             LOG.debug('        Repr: {0:}'.format(repr(self)))
 
+
+
     def makeConnection(self, p_transport):
-        """
+        """ Override
         Emit a helpful log message when the connection is made.
-        Required to be here - passes back the transport apparently.
+        Required to be here - passes back the transport.
         """
         self.m_transport = p_transport
         if g_debug >= 2:
-            LOG.debug('==105== Dispatch - makeConnection')
+            LOG.debug('==118== Dispatcher - makeConnection(Transport) to:{0:}'.format(p_transport.getPeer()))
             LOG.debug('        Transport:{0:}'.format(p_transport))
+        return AMP.makeConnection(self, p_transport)
+
+
+
+    def connectionLost(self, p_reason):
+        """ Override
+        Clean up the connection.
+        """
+        self.m_transport = None
+        if g_debug >= 1:
+            LOG.debug('==129== Dispatcher - connectionLost(Reason)')
+            LOG.debug('        ERROR: {0:}\n'.format(p_reason))
+
+
+
+    def dataReceived(self, data):
+        """ Override
+        Either parse incoming data as AmpBoxes or relay it to our nested protocol.
+        """
+        LOG.debug('==138== Dispatcher - dataReceived(Data) - {0:}'.format(PrintBox(data)))
+        return AMP.dataReceived(self, data)
 
     def connectionMade(self):
-        """Somebody connected to us...
+        """
+        Somebody connected to us...
         This may be considered the initializer of the protocol, because it is called when the connection is completed.
         For clients, this is called once the connection to the server has been established.
         For servers, this is called after an accept() call stops blocking and a socket has been received.
         If you need to send any greeting or initial message, do it here.
         """
         if g_debug >= 1:
-            LOG.debug('==116== Dispatch - connectionMade')
+            LOG.debug('==150== Dispatcher - connectionMade()')
         pass
-
-
-    def connectionLost(self, p_reason):
-        """Clean up the connection.
-        """
-        if g_debug >= 1:
-            LOG.debug('==124== ServerProtocol - connectionLost')
-            LOG.debug('        ERROR: {0:}\n'.format(p_reason))
-
 
 
 
     def startReceivingBoxes(self, p_boxSender):
+        """ Override
+        The given boxSender is going to start calling boxReceived on this BoxDispatcher.
+
+        @param boxSender: The L{IBoxSender} to send command responses to.
+        """
         if g_debug >= 1:
-            LOG.debug('==132== Dispatch - Start Receiving boxes')
+            LOG.debug('==160== Dispatch - StartReceivingBoxes(Sender)')
             LOG.debug('        Sender:{0:}'.format(p_boxSender))
         self.boxSender = p_boxSender
 
 
+
     def ampBoxReceived(self, p_box):
+        """ Override
+        An AmpBox was received, representing a command, or an answer to a previously issued command (either successful or erroneous).
+        Respond to it according to its contents.
+        """
         if g_debug >= 1:
-            LOG.debug('==139== Dispatch - Received box ')
+            LOG.debug('==169== Dispatch - BoxReceived(Box)')
             LOG.debug('        Box:{0:}'.format(p_box))
-        self.boxSender.sendBox(p_box)
+        self.sendBox(p_box)
+        # self.boxSender.sendBox(p_box)
+
 
 
     def stopReceivingBoxes(self, p_reason):
+        """ Override
+        No further boxes will be received here. Terminate all currently oustanding command deferreds with the given reason.
+        """
         if g_debug >= 1:
-            LOG.debug('==146== Dispatch - Stop Receiving boxes')
+            LOG.debug('==179== Dispatch - StopReceivingBoxes(Reason)')
             LOG.debug('        Reason: {0:}'.format(p_reason))
         self.boxSender = None
 
+
+
     @NodeInformationCommand.responder
-    def receive_NodeInformation(self, Name = None, Active = None, Address = None, Role = None, UUID = None):
+    def receive_NodeInformation(self, Name = None):
         """
         The responder expects to be called with a serialized box.
         It will then
@@ -159,16 +202,17 @@ class DomainBoxDispatcher_AmpProtocol(AMP):
             and then return that serialized form.
         """
         if g_debug >= 1:
-            LOG.debug('==162== Dispatch - receive_NodeInformation - RECEIVED')
+            LOG.debug('==197== Dispatch - receive_NodeInformation() - RECEIVED')
         _l_box = NodeInformationCommand.makeArguments({'Name': 'AAA'})
         l_ret = dict(Name = Name, Answer = 'Got it ok')
         return l_ret
     # NodeInformationCommand.responder(receive_NodeInformation)
 
 
+
     def update_NodeInformation(self, p_box):
         if g_debug >= 1:
-            LOG.debug('==178== Dispatch - update_NodeInformation')
+            LOG.debug('==208== Dispatch - update_NodeInformation(Box)')
             LOG.debug('        Box: {0:}'.format(vars(p_box)))
         for l_node in self.m_pyhouse_obj.Computer.Nodes.itervals():
             if l_node.Name == p_box.Name:
@@ -176,28 +220,46 @@ class DomainBoxDispatcher_AmpProtocol(AMP):
                 l_node.UUID = p_box.UUID
                 l_node.Active = p_box.Active
 
+
+
     # @NodeInformationCommand.responder
     def response_NodeOnfo(self):
-        LOG.debug('==181== LocatorClass - received  remote server.')
+        LOG.debug('==219== Dispatch - received  remote server.')
         l_ret = dict(Name = 'abc', Answer = 'Yes')
         return l_ret
+
+    def sendBox(self, box):
+        """ Override
+        """
+        AMP.sendBox(self, box)
+
+
+
+
+
+
+
 
 
 class AmpServerFactory(ServerFactory):
     """
     """
+
     def __init__(self, p_pyhouses_obj):
         self.m_pyhouse_obj = p_pyhouses_obj
 
+
+
     def buildProtocol(self, _p_address_tupple):
-        l_protocol = NodeDomainServerProtocol(self.m_pyhouse_obj)
+        # l_protocol = NodeDomainServerProtocol(self.m_pyhouse_obj)
+        l_protocol = DomainAmp(self.m_pyhouse_obj)
         self.protocol = l_protocol
         if g_debug >= 4:
-            LOG.debug('==195== AmpServerFactory.buildProtocol()')
+            LOG.debug('==246== AmpServerFactory.buildProtocol()')
         return l_protocol
 
 
-class NodeDomainServerProtocol(DomainBoxDispatcher_AmpProtocol):
+class NodeDomainServerProtocol(DomainAmp):
     """
     Implement dataReceived(data) to handle both event-based and synchronous input.
     output can be sent through the 'transport' attribute.
@@ -205,19 +267,24 @@ class NodeDomainServerProtocol(DomainBoxDispatcher_AmpProtocol):
     When BinaryBoxProtocol is connected to a transport, it calls startReceivingBoxes on its IBoxReceiver
     with itself as the IBoxSender parameter.
     """
+
     def __init__(self, p_pyhouse_obj):
-        LOG.debug('==208== NodeDomainServerProtocol()')
+        LOG.debug('==245== NodeDomainServerProtocol()')
         self.m_pyhouse_obj = p_pyhouse_obj
-        self.m_disp = DomainBoxDispatcher_AmpProtocol(p_pyhouse_obj)
+        self.m_disp = DomainAmp(p_pyhouse_obj)
         if g_debug >= 1:
-            LOG.debug('==212== ServerProtocol() initialized')
+            LOG.debug('==249== ServerProtocol() initialized')
             LOG.debug('        Dispatch:{0:}'.format(self.m_disp))
+
+
 
     def _extract_arg(self, p_msg):
         l_len = ord(p_msg[0]) * 256 + ord(p_msg[1])
         l_arg = p_msg[2:l_len + 2]
         l_rest = p_msg[l_len + 2:]
         return l_arg, l_rest
+
+
 
     def _make_dict_from_message(self, p_msg):
         l_dict = {}
@@ -228,25 +295,31 @@ class NodeDomainServerProtocol(DomainBoxDispatcher_AmpProtocol):
             l_dict[l_key] = l_arg
         return l_dict
 
+
+
     def dataReceived(self, p_data):
         if g_debug >= 1:
-            LOG.debug('==237== ServerProtocol - data rxed')
+            LOG.debug('==275== ServerProtocol - dataReceived(Data)')
             LOG.debug('        Data rxed: {0:}'.format(PrintBox(p_data)))
         l_dict = self._make_dict_from_message(p_data)
         PrettyPrintAny(l_dict, 'NodeDomain - DataReceived - Dict', 120)
 
 
+
     def cb_got_result12(self, p_result):
         if g_debug >= 1:
-            LOG.debug('==240== ServerProtocol - ConnectionMade cb_got_result')
+            LOG.debug('==284== ServerProtocol - ConnectionMade cb_got_result(Result)')
             LOG.debug('        Client Addr:{0:}'.format(self.m_address))
             LOG.debug('        Result:{0:}'.format(p_result))
             LOG.debug('        Transport{0:}'.format(self.transport))
 
+
+
     def eb_err12(self, p_ConnectionDone):
-        LOG.error('==246== ServerProtocol - ConnectionMade')
+        LOG.error('==292== ServerProtocol - ConnectionMade eb_G')
         LOG.error('        Address: {0:}'.format(self.m_address))
         LOG.error('        ERROR: {0:}\n'.format(p_ConnectionDone))
+
 
 
     def connectionMade(self):
@@ -257,7 +330,7 @@ class NodeDomainServerProtocol(DomainBoxDispatcher_AmpProtocol):
         If you need to send any greeting or initial message, do it here.
         """
         if g_debug >= 1:
-            LOG.debug('==259== ServerProtocol - ConnectionMade')
+            LOG.debug('==306== ServerProtocol - ConnectionMade')
             # LOG.debug('    self = {0:}\n'.format(vars(self)))
 
 
@@ -272,15 +345,19 @@ class IrPacketError(Exception): pass
 
 class AmpClient(object):
 
+
+
     def cb_sendInfo(self, p_ampProto):
         l_node = self.m_pyhouse_obj.Computer.Nodes[0]
         if g_debug >= 4:
-            LOG.debug('==277== Client - sending our node info to remote server.')
+            LOG.debug('==326== Client - sending our node info to remote server.')
             LOG.debug('        Protocol:{0:}   Addr:{1:}'.format(p_ampProto, self.m_address))
         l_ret = p_ampProto.callRemote(NodeInformationCommand,
                         Name = l_node.Name, Active = str(l_node.Active), Address = l_node.ConnectionAddr_IPv4,
                         NodeRole = int(l_node.NodeRole), UUID = "01234567-1234-2345-3456-01234567890ab")
         return l_ret
+
+
 
     def eb_send_info(self, p_reason):
         LOG.warn('WARNING - Failed to create a client to send to another node.  {0:}'.format(p_reason))
@@ -293,7 +370,7 @@ class AmpClient(object):
         self.m_pyhouse_obj = p_pyhouses_obj
         self.m_address = p_address
         l_endpoint = TCP4ClientEndpoint(p_pyhouses_obj.Twisted.Reactor, p_address, AMP_PORT)
-        LOG.debug('==295== About to create client to {0:}'.format(p_address))
+        LOG.debug('==346== About to create client to {0:}'.format(p_address))
         l_defer = l_endpoint.connect(ClientFactory.forProtocol(AMP))
         l_defer.addCallback(self.cb_sendInfo)
         l_defer.addErrback(self.eb_send_info)
