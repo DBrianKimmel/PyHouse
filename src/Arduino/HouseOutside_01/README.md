07da391275981f0f3ea66bd35ccaf31982597b1a
DBrianKimmel <d.briankimmel@gmail.com>
2015-04-30 20:46:47 -0400

    WIP

--- src/Arduino/HouseOutside_01/README.md
+++ src/Arduino/HouseOutside_01/README.md
@@ -1,13 +1,14 @@
 HouseOutside_01
-:===============:
+===============
 
 This is a module for Outside the home.
+
 It contains:
--Arduino
--ESP8266 WiFi transceiver
--Light Level Sensor
--DH22 Temperature / Humidity Sensor
--PIR People Sensor
+- Arduino
+- ESP8266 WiFi transceiver
+- Light Level Sensor
+- DH22 Temperature / Humidity Sensor
+- PIR People Sensor
 
 Like all other PyHouse sensors, it also has an MQTT component.
 
--- src/Arduino/WiFi_01/WiFi_01.ino
+++ src/Arduino/WiFi_01/WiFi_01.ino
@@ -5,10 +5,9 @@
  * 
  *This is a base line project 
  */
- #include<stdlib.h>
+#include<stdlib.h>
 #include <SoftwareSerial.h>
 SoftwareSerial monitor(5, 7); // RX, TX
-
 
 // Include various modules here
 // Module WiFi_01 - Connect to WiFi access point
@@ -20,74 +19,74 @@ SoftwareSerial monitor(5, 7); // RX, TX
 #define WIFI_PASSWORD    "123456789"
 ESP8266 wifi(monitor);    // Instantiate
 void print_bool(char* p_name, bool p_value) {
-    Serial.print(p_name);
-    if (p_value) Serial.println(" Success.");
-    else Serial.println(" Failure.");
+	Serial.print(p_name);
+	if (p_value)
+		Serial.println(" Success.");
+	else
+		Serial.println(" Failure.");
 }
 void wifiRestart() {
-    bool l_erc = wifi.restart();
-    print_bool("Restart: ", l_erc);
+	bool l_erc = wifi.restart();
+	print_bool("Restart: ", l_erc);
 }
 void wifiKick() {
-    bool l_erc = wifi.kick();
-    print_bool("Kick: ", l_erc);
+	bool l_erc = wifi.kick();
+	print_bool("Kick: ", l_erc);
 }
 void wifiGetVersion() {
-    String l_version = wifi.getVersion().c_str();
-    Serial.print("Firmware Version: ");
-    Serial.println(l_version);
+	String l_version = wifi.getVersion().c_str();
+	Serial.print("Firmware Version: ");
+	Serial.println(l_version);
 }
 void wifiOprToStation() {
-    bool l_erc = wifi.setOprToStation();
-    print_bool("Set Operation Mode to Station: ", l_erc);
+	bool l_erc = wifi.setOprToStation();
+	print_bool("Set Operation Mode to Station: ", l_erc);
 }
 void wifiOprToSoftAP() {
-    bool l_erc = wifi.setOprToSoftAP();
-    print_bool("Set Operation Mode to SoftAP: ", l_erc);
+	bool l_erc = wifi.setOprToSoftAP();
+	print_bool("Set Operation Mode to SoftAP: ", l_erc);
 }
 void wifiOprToStationSoftAP() {
-    bool l_erc = wifi.setOprToStationSoftAP();
-    print_bool("Set Operation Mode to StationSoftAP: ", l_erc);
+	bool l_erc = wifi.setOprToStationSoftAP();
+	print_bool("Set Operation Mode to StationSoftAP: ", l_erc);
 }
 void wifiJoinAP(String p_ssid, String p_password) {
-    bool l_erc = wifi.joinAP(WIFI_SSID, WIFI_PASSWORD);
-    print_bool("Join AP: ", l_erc);
+	bool l_erc = wifi.joinAP(WIFI_SSID, WIFI_PASSWORD);
+	print_bool("Join AP: ", l_erc);
 }
 void wifiListAPs() {
-    String l_ret = wifi.getAPList();
-    Serial.println("List of APs: ");
-    Serial.println(l_ret);
-    Serial.println("__________");
+	String l_ret = wifi.getAPList();
+	Serial.println("List of APs: ");
+	Serial.println(l_ret);
+	Serial.println("__________");
 }
 void wifi_setup() {
-    Serial.print("wifi_setup begin\n");
-    wifiRestart();
-    wifiKick();
-    wifiGetVersion();
-    wifiOprToStation();
-    wifiOprToSoftAP();
-    wifiOprToStationSoftAP();
-    wifiJoinAP(WIFI_SSID, WIFI_PASSWORD);
-    wifiListAPs();
-    // Serial.print("setup end\r\n");
+	Serial.print("wifi_setup begin\n");
+	wifiRestart();
+	wifiKick();
+	wifiGetVersion();
+	wifiOprToStation();
+	wifiOprToSoftAP();
+	wifiOprToStationSoftAP();
+	wifiJoinAP(WIFI_SSID, WIFI_PASSWORD);
+	wifiListAPs();
+	// Serial.print("setup end\r\n");
 }
 void wifi_loop() {
 }
 // Module END - WiFi
 
-
-
 // Standard setup and loop
 void setup() {
-    monitor.begin(9600);
-    Serial.begin(9600);
-    Serial.println("WiFi_01\n");
-    // Include vaarious module setups here.
-    wifi_setup();
+	monitor.begin(9600);
+	Serial.begin(9600);
+	Serial.println("WiFi_01\n");
+	// Include vaarious module setups here.
+	wifi_setup();
 }
 void loop() {
-    // Include various module loops here.
-    wifi_loop();
+	// Include various module loops here.
+	wifi_loop();
 }
 
 // ### END DBK
