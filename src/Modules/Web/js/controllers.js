1392dc3d521ce542712e3d804f4714fda10c7532
DBrianKimmel <d.briankimmel@gmail.com>
2015-08-26 13:39:20 -0400

    Fixed uninitialized var in adding new controller web js

--- src/Modules/Web/js/controllers.js
+++ src/Modules/Web/js/controllers.js
@@ -98,15 +98,19 @@ helpers.Widget.subclass(controllers, 'ControllersWidget').methods(
     function handleMenuOnClick(self, p_node) {
         var l_ix = p_node.name;
         var l_name = p_node.value;
+        var l_obj;
 		globals.House.ControllerIx = l_ix;
 		globals.House.ControllerName = l_name;
         if (l_ix <= 1000) {  // One of the controller buttons
             showDataEntryFields(self);
-			var l_obj = globals.House.HouseObj.Controllers[l_ix];
+			l_obj = globals.House.HouseObj.Controllers[l_ix];
 			globals.House.ControllerObj = l_obj;
 			globals.House.Self = self;
             self.buildLcarDataEntryScreen(l_obj, 'handleDataOnClick');
         } else if (l_ix == 10001) {  // The 'Add' button
+        	l_obj = self.createEntry()
+			globals.House.ControllerObj = l_obj;
+			globals.House.Self = self;
             showDataEntryFields(self);
 			var l_ent = self.createEntry();
 			self.buildLcarDataEntryScreen(l_ent, 'handleDataOnClick');
@@ -121,6 +125,7 @@ helpers.Widget.subclass(controllers, 'ControllersWidget').methods(
 	 * Build a screen full of data entry fields.
 	 */
 	function buildLcarDataEntryScreen(self, p_entry, p_handler){
+		Divmod.debug('---', 'controllers.buildLcarDataEntryScreen() called ');
 		var l_obj = arguments[1];
 		var l_html = build_lcars_top('Controller Data', 'lcars-salmon-color');
 		l_html += build_lcars_middle_menu(40, self.buildEntry(l_obj, p_handler));
@@ -136,9 +141,13 @@ helpers.Widget.subclass(controllers, 'ControllersWidget').methods(
 		else if (p_obj.DeviceFamily === 'UPB')
         	l_html = buildUpbPart(self, p_obj, l_html);
 		else
-			Divmod.debug('---', 'ERROR - controllers.buildAllParts() Family = ' + p_obj.DeviceFamily);
+			Divmod.debug('---', 'ERROR - controllers.buildEntry()  Family = ' + p_obj.DeviceFamily);
+		//
 		if (p_obj.InterfaceType === 'Serial')
 			l_html = buildSerialPart(self, p_obj, l_html);
+		else
+			Divmod.debug('---', 'ERROR - controllers.buildEntry()  Interface = ' + p_obj.InterfaceType);
+		//
 		l_html += buildLcarEntryButtons(p_handler);
 		return l_html;
 	},
@@ -158,7 +167,8 @@ helpers.Widget.subclass(controllers, 'ControllersWidget').methods(
 		Divmod.debug('---', 'controllers.interfaceChanged() was called.');
 		var l_obj = globals.House.ControllerObj;
 		var l_self = globals.House.Self;
-		// console.log("controllers.interfaceChanged() - l_self = %O", l_self);
+		console.log("controllers.interfaceChanged() - l_obj = %O", l_obj);
+		console.log("controllers.interfaceChanged() - l_self = %O", l_self);
 		l_obj.InterfaceType = fetchSelectWidget(l_self, 'InterfaceType');
 		l_self.buildLcarDataEntryScreen(l_obj, 'handleDataOnClick');
 	},
@@ -186,6 +196,7 @@ helpers.Widget.subclass(controllers, 'ControllersWidget').methods(
         l_data.LightingType = 'Controller';
         if (l_data.DeviceFamily === 'Insteon')
         	l_data = createInsteonEntry(self, l_data);
+        //
         if (l_data.InterfaceType === 'Serial')
         	l_data = createSerialEntry(self, l_data);
         return l_data;
