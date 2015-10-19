501b54d58aae96a1fc2b0bfe05595c5099e0487b
DBrianKimmel <d.briankimmel@gmail.com>
2015-10-19 13:12:13 -0400

    Fix filter - it was not allowing anything to be logged Something must have changed in some twisted/python update.

--- src/Modules/Core/setup_logging.py
+++ src/Modules/Core/setup_logging.py
@@ -16,6 +16,7 @@ Log file paths are hard coded therefore.
 Log directories must exist and be writable by the PyHouse process as it begins.
 
 """
+from Modules.Utilities.debug_tools import PrettyFormatAny
 
 LOGGER_NAME = 'PyHouse                '
 LOGGER_NAME_TWISTED = 'PyHouse.Twisted.....   '
@@ -32,33 +33,38 @@ import logging.config
 from twisted.python import log
 
 
-
 class DropHttpFilter(object):
-    """This will filter out all the HTTP log messages added by twisted (Athena/Nevow).
+    """This will filter out the HTTP log messages added by twisted (Athena/Nevow).
     """
 
     def __init__(self, p_param = None):
+        """ Save the list of strings to filter the log message
+        """
         self.m_param = p_param
 
     def filter(self, p_record):
+        l_allow = True
         if self.m_param is None:
-            allow = True
+            return l_allow
         else:
-            # allow = self.m_param not in p_record.msg
-            allow = False
+            for l_entry in self.m_param:
+                if l_entry in p_record.msg:
+                    l_allow = False
+            # l_allow = self.m_param not in p_record.msg
+            # allow = False
         # if allow:
         #    p_record.msg = 'changed: ' + p_record.msg
-        return allow
+        return l_allow
 
 
 LOGGING_DICT = {
-    'version' : 1,
+    'version'                  : 1,
     'disable_existing_loggers' : False,
 
-    'filters': {
-        'http': {
-            '()': DropHttpFilter,
-            'p_param': '/transport ',
+    'filters' : {
+        'http' : {
+            '()'      : DropHttpFilter,
+            'p_param' : ['/transport HTTP/', '/jsmodule/'],
         }
     },
 
@@ -72,41 +78,50 @@ LOGGING_DICT = {
     },
 
     'handlers' : {
+        'debug'   : {
+            'class'       : 'logging.handlers.TimedRotatingFileHandler',
+            'level'       : 'DEBUG',
+            'formatter'   : 'verbose',
+            'filename'    : '/var/log/pyhouse/debug',
+            'when'        : 'midnight',
+            'filters'     : ['http'],
+            'backupCount' : '20',
+        },
+        'error'   : {
+            'class'       : 'logging.handlers.TimedRotatingFileHandler',
+            'level'       : 'ERROR',
+            'formatter'   : 'standard',
+            'filename'    : ERROR_LOG_LOCTION,
+            'when'        : 'midnight',
+            'backupCount' : '20',
+        },
         'console' : {
-            'class':'logging.StreamHandler',
-            'level' : 'DEBUG',
-            'formatter' : 'standard',
-        },
-        'debug' : {
-            'class':'logging.handlers.TimedRotatingFileHandler',
-            'level' : 'DEBUG',
-            'filters': ['http'],
-            'formatter' : 'verbose',
-            'filename' : DEBUG_LOG_LOCTION,
-            'when' : 'midnight',
-        },
-        'error' : {
-            'class':'logging.handlers.TimedRotatingFileHandler',
-            'level' : 'ERROR',
-            'formatter' : 'standard',
-            'filename' : ERROR_LOG_LOCTION,
-            'when' : 'midnight',
+            'class'       : 'logging.StreamHandler',
+            'level'       : 'DEBUG',
+            'filters'     : ['http'],
+            'formatter'   : 'standard',
         },
     },
 
-    'root': {
-        'handlers': ['debug', 'error'],
-        'level' : 'DEBUG',
+    'root' : {
+        'handlers' : ['debug', 'error'],
+        'level'    : 'DEBUG',
     },
-    'PyHouse': {
-        'handlers': ['console'],
-        'level' : 'DEBUG',
+    'PyHouse' : {
+        'handlers' : ['console'],
+        'level'    : 'DEBUG',
     },
 }
 
 logging.getLogger(LOGGER_NAME)
 logging.config.dictConfig(LOGGING_DICT)
-logging.info('PyHouse Starting\n')
+logging.debug('PyHouse Debug Starting\n')
+logging.info('PyHouse Info Starting\n')
+logging.warn('PyHouse Warn Starting\n')
+logging.error('PyHouse Error Starting\n')
+logging.critical('PyHouse Critical Starting\n')
+print('setup_logging-109')
+print(PrettyFormatAny.form(logging, 'Logging'))
 
 l_observer = log.PythonLoggingObserver(loggerName = LOGGER_NAME_TWISTED)
 l_observer.start()
@@ -136,6 +151,7 @@ class API(object):  # To remove eclipse warnings.
 
     def __init__(self, p_pyhouse_obj):
         self.m_pyhouse_obj = p_pyhouse_obj
+        print('setup_logging-138')
 
     def Start(self):
         # PrettyPrintAny(self.m_pyhouse_obj.Twisted, 'PyHouseObj.Twisted')
