86b8c9c839a122e136fd14f60b27d4d594fb5967
DBrianKimmel <d.briankimmel@gmail.com>
2015-10-22 15:45:01 -0400

    Dict wrong

--- src/Modules/Computer/Mqtt/mqtt_client.py
+++ src/Modules/Computer/Mqtt/mqtt_client.py
@@ -169,9 +169,9 @@ class API(Util):
         if l_topic[0] == 'login':
             l_logmsg = 'Login: {}'.format(PrettyFormatAny.form(l_message, 'Message', 80))
         elif l_topic[0] == 'lighting':
-            l_logmsg = '\n\tName: {}\n\tRoom: {}\n\tLevel: {}'.format(l_message.Name, l_message.RoomName, l_message.CurLevel)
+            l_logmsg = '\n\tName: {}\n\tRoom: {}\n\tLevel: {}'.format(l_message['Name'], l_message['RoomName'], l_message['CurLevel'])
         elif l_topic[0] == 'schedule' and l_topic[1] == 'execute':
-            l_logmsg = '\n\tType: {}\n\tRoom: {}\n\tLight: {}\n\tLevel: {}'.format(l_message.ScheduleType, l_message.RoomName, l_message.LightName, l_message.CurLevel)
+            l_logmsg = '\n\tType: {}\n\tRoom: {}\n\tLight: {}\n\tLevel: {}'.format(l_message['ScheduleType'], l_message['RoomName'], l_message['LightName'], l_message['CurLevel'])
         else:
             l_logmsg = 'OTHER: {}'.format(PrettyFormatAny.form(l_message, 'Message', 80))
         LOG.info(l_logmsg)
