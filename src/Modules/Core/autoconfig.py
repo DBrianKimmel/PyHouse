c794354c64bbf86e00ea88aee9bd1a294a940940
DBrianKimmel <d.briankimmel@gmail.com>
2015-11-03 11:19:49 -0500

    Updating

---
+++ src/Modules/Core/autoconfig.py
@@ -0,0 +1,22 @@
+"""
+@name:      C:/Users/briank/workspace/PyHouse/src/Modules/Core/autoconfig.py
+@author:    D. Brian Kimmel
+@contact:   D.BrianKimmel@gmail.com
+@copyright: (c) 2015-2015 by D. Brian Kimmel
+@license:   MIT License
+@note:      Created on Nov 2, 2015
+@Summary:
+
+Autoconfigure a new PyHouse installation.
+
+"""
+
+FILE = '/etc/pyhouse/config.xml'
+
+class API(object):
+    """
+    """
+
+
+
+# ## END DBK
