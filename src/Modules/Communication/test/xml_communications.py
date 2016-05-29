53e37acf8873d415c8c48b2087a2bf5c9c938a00
D. Brian Kimmel <d.briankimmel@gmail.com>
2016-05-28 20:11:40 -0400

    WIP

--- src/Modules/Communication/test/xml_communications.py
+++ src/Modules/Communication/test/xml_communications.py
@@ -2,7 +2,7 @@
 @name:      PyHouse/src/Modules/Communication/test/xml_communications.py
 @author:    D. Brian Kimmel
 @contact:   D.BrianKimmel@gmail.com
-@copyright: (c) 2014-2015 by D. Brian Kimmel
+@copyright: (c) 2014-2016 by D. Brian Kimmel
 @license:   MIT License
 @note:      Created on Nov 17, 2014
 @Summary:
@@ -10,7 +10,16 @@
 """
 
 
-EMAIL_XML = """
+L_COMMUNICATION_START = '<CommunicationSection>'
+L_COMMUNICATION_END = '</CommunicationSection>'
+
+L_EMAIL_START = '<EmailSection>'
+L_EMAIL_END = '</EmailSection>'
+
+L_TWITTER_START = '<TwitterSection>'
+L_TWITTER_END = '</TwitterSection>'
+
+XML_EMAIL = """
     <EmailSection>
         <EmailFromAddress>mail.sender@Gmail.Com</EmailFromAddress>
         <EmailToAddress>mail.receiver@Gmail.Com</EmailToAddress>
@@ -22,9 +31,25 @@ EMAIL_XML = """
 """
 
 
+TESTING_CONSUMER_KEY = 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
+TESTING_CONSUMER_SECRET = '1234567890ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
+TESTING_ACCESS_TOKEN = 'ZYXWVUTSRQPONMLKJIHFEDCBA'
+TESTING_ACCESS_TOKEN_SECRET = '0987654321ZYXWVUTSRQPONMLKJIHFEDCBA'
+
+L_CONSUMER_KEY = '    <ConsumerKey>' + TESTING_CONSUMER_KEY + '</ConsumerKey>'
+L_CONSUMER_SECRET = '    <ConsumerSecret>' + TESTING_CONSUMER_SECRET + '</ConsumerSecret>'
+
+
+XML_TWITTER = '\n'.join([
+    L_TWITTER_START,
+    L_TWITTER_END
+])
 
 XML_COMMUNICATION = '\n'.join([
-    EMAIL_XML
+    L_COMMUNICATION_START,
+    XML_EMAIL,
+    XML_TWITTER,
+    L_COMMUNICATION_END
 ])
 
 # ## END DBK
